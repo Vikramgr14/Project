d9882a3536eaf39516ba8197697804660289eb8a
Vikramgr14 <Vikram.GRathod@cognizant.com>
2026-05-27 21:18:26 +0530

    Final Commit

--- src/test/java/com/hackathonproject/base/BaseTest.java
+++ src/test/java/com/hackathonproject/base/BaseTest.java
@@ -2,6 +2,8 @@ package com.hackathonproject.base;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
@@ -14,55 +16,59 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 
 import com.hackathonproject.util.ConfigReader;
 
-import java.io.File;
 import java.time.Duration;
 
 public class BaseTest {
 
     private static final Logger log = LogManager.getLogger(BaseTest.class);
-
     private static WebDriver driver;
 
+    //It opens and closes the browser for each scenario.
     public static WebDriver getDriver() {
         return driver;
     }
 
     @Before
     public void setUp() {
+
         String browser = com.hackathonproject.runner.TestRunner.getBrowserName() != null
                 ? com.hackathonproject.runner.TestRunner.getBrowserName()
                 : ConfigReader.get("browser");
 
         log.info("Initializing Browser: {}", browser);
 
-        if (browser.equalsIgnoreCase("chrome")) {
-            String path = requireDriver(ConfigReader.get("chrome.driver.path"), "Chrome");
-            System.setProperty("webdriver.chrome.driver", path);
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--start-maximized");
-            driver = new ChromeDriver(options);
+        switch (browser.toLowerCase()) {
 
-        } else if (browser.equalsIgnoreCase("edge")) {
-            String path = requireDriver(ConfigReader.get("edge.driver.path"), "Edge");
-            System.setProperty("webdriver.edge.driver", path);
-            EdgeOptions options = new EdgeOptions();
-            options.addArguments("--start-maximized");
-            driver = new EdgeDriver(options);
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--start-maximized");
+                driver = new ChromeDriver(chromeOptions);
+                break;
 
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            String path = requireDriver(ConfigReader.get("firefox.driver.path"), "Firefox");
-            System.setProperty("webdriver.gecko.driver", path);
-            FirefoxOptions options = new FirefoxOptions();
-            options.addArguments("--start-maximized");
-            driver = new FirefoxDriver(options);
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--start-maximized");
+                driver = new EdgeDriver(edgeOptions);
+                break;
 
-        } else {
-            log.error("Browser not supported: {}", browser);
-            throw new IllegalArgumentException("Browser not supported: " + browser);
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--start-maximized");
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+
+            default:
+                log.error("Browser not supported: {}", browser);
+                throw new IllegalArgumentException("Browser not supported: " + browser);
         }
 
+        // Timeouts
         driver.manage().timeouts().implicitlyWait(
                 Duration.ofSeconds(ConfigReader.getInt("implicit.wait")));
+
         driver.manage().timeouts().pageLoadTimeout(
                 Duration.ofSeconds(ConfigReader.getInt("page.load.timeout")));
 
@@ -77,20 +83,5 @@ public class BaseTest {
             driver.quit();
             driver = null;
         }
-    }
-
-    private String requireDriver(String path, String browserLabel) {
-        if (path == null || path.isBlank()) {
-            throw new IllegalStateException(
-                    browserLabel + " driver path is not configured. Check config.properties.");
-        }
-        File f = new File(path);
-        if (!f.exists()) {
-            throw new IllegalStateException(
-                    browserLabel + " driver not found at: " + f.getAbsolutePath()
-                            + "\nPlace the executable in src/test/resources/drivers/ "
-                            + "and update config.properties.");
-        }
-        return path;
     }
 }
